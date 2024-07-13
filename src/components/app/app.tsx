63f6bfb6c57080e4c6a6166493e56825c07142a3
asdqdsa <asdqdsa@protonmail.com>
2024-07-13 18:44:22 +0400

    feat: add initial routes to app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,37 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
+import { Routes, Route, Link } from 'react-router-dom';
+
 import { AppHeader } from '@components';
+import { Feed } from '@pages';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    {/* <ConstructorPage /> */}
+
+    <Routes>
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+    </Routes>
   </div>
 );
 
