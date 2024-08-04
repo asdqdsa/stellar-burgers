0522f30d3e6657c3b6a30640a2e1df48e8cde986
asdqdsa <asdqdsa@protonmail.com>
2024-08-04 17:52:47 +0400

    test: init jest; test(unit): add rootReducer state

---
+++ src/services/store.test.ts
@@ -0,0 +1,22 @@
+import { rootReducer } from './store';
+import ingredientsSlice from './slices/burgerIngredientsSlice';
+import feedSlice from './slices/feedSlice';
+import burgerConstructorSlice from './slices/burgerConstructorSlice';
+import profileSlice from './slices/profileSlice';
+import orderSlice from './slices/orderSlice';
+
+describe('Init store/rootReducer', () => {
+  it('checks correct initial state for each slice', () => {
+    const initialState = rootReducer(undefined, { type: 'UNKNOWN_ACTION' });
+
+    expect(initialState).toEqual({
+      ingredients: ingredientsSlice(undefined, { type: 'UNKNOWN_ACTION' }),
+      feed: feedSlice(undefined, { type: 'UNKNOWN_ACTION' }),
+      burgerConstructor: burgerConstructorSlice(undefined, {
+        type: 'UNKNOWN_ACTION'
+      }),
+      profile: profileSlice(undefined, { type: 'UNKNOWN_ACTION' }),
+      order: orderSlice(undefined, { type: 'UNKNOWN_ACTION' })
+    });
+  });
+});
